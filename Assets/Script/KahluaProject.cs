1e88203314fe23616fa1cdd43827fd9c213ecafa
zinine <zinine@naver.com>
2019-08-20 21:34:39 +0900

    test commit

---
+++ Assets/Script/KahluaProject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KahluaProject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO: okay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ab aaa = new ab();
+    }
+
+    private class ab
+    {
+        int a;
+    }
+}
